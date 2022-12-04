24b35404e2ff0b082c8b881221e3cdc20f51e8d9
Tobias Blum <tobias.blum@besonet.ch>
2022-12-04 22:05:25 +0100

    Add handling for incoming mqtt data

--- server/plugins/connectors/mqtt.ts
+++ server/plugins/connectors/mqtt.ts
@@ -14,9 +14,41 @@ export default defineNitroPlugin((nitroApp) => {
     });
   });
 
-  client.on("message", function (topic, message) {
-    // message is Buffer
-    console.log(message.toString());
-    // client.end()
+  client.on("message", async function (topic, message, packet) {
+    let db = globalThis.db;
+
+    // extract data from topic
+    let [deviceId, tagId] = topic
+      .replace("tbl/nuxt/", "")
+      .split("/")
+      .slice(0, 2);
+
+    // check if device and tag exists - e.g. `clao804lj0002nvew7xfjqlt6/clao804lj0003nvewpsu2cj2h/`
+    let device = await db.device.findFirst({
+      include: { tags: true },
+      where: {
+        id: deviceId,
+        tags: { some: { id: tagId } },
+      },
+    });
+    if (!device) {
+      console.log(
+        `Got message which does not belong to device and tag combination. ${deviceId} - ${tagId}`
+      );
+      return;
+    }
+
+    // TODO: validate mqtt payload
+
+    // build mqtt payload
+    let payload = {
+      timeUTC: Date.now().toString(),
+      device: deviceId,
+      tag: tagId,
+      data: JSON.parse(message.toString()),
+    };
+
+    // send payload to interpreter service (nuxt hooks) e.g. `connector:mqtt:new-data` with payload {device: "", tag: "", timeUTC: "", data: {}}
+    nitroApp.hooks.callHook("connector:mqtt:new-data", payload);
   });
 });
