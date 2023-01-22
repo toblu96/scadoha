b418a0024bebcfb8b043dce6e84d91f250f34fc2
Tobias Blum <tobias.blum@besonet.ch>
2023-01-22 15:34:10 +0100

    Add pocketbase plugin

---
+++ plugins/pocketbase.ts
@@ -0,0 +1,42 @@
+// plugins/pocketbase.js
+import PocketBase, { Admin, Record } from "pocketbase";
+
+interface ICookie {
+  token: string;
+  model: Record | Admin | null;
+}
+
+export default defineNuxtPlugin(async () => {
+  const pb = new PocketBase("http://127.0.0.1:8090");
+
+  const cookie = useCookie<ICookie>("pb_auth", {
+    path: "/",
+    secure: true,
+    sameSite: "strict",
+    httpOnly: false, // change to "true" if you want only server-side access
+    maxAge: 604800,
+  });
+
+  // load the store data from the cookie value
+  pb.authStore.save(cookie.value?.token || "", cookie.value?.model || null);
+
+  // send back the default 'pb_auth' cookie to the client with the latest store state
+  pb.authStore.onChange(() => {
+    cookie.value = {
+      token: pb.authStore.token,
+      model: pb.authStore.model,
+    };
+  });
+
+  try {
+    // get an up-to-date auth store state by verifying and refreshing the loaded auth model (if any)
+    pb.authStore.isValid && (await pb.collection("users").authRefresh());
+  } catch (_) {
+    // clear the auth store on failed refresh
+    pb.authStore.clear();
+  }
+
+  return {
+    provide: { pb },
+  };
+});
