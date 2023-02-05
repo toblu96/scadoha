588996813fbe3461829901b27c127280694dd0f5
Tobias Blum <tobias.blum@besonet.ch>
2023-02-05 14:28:59 +0100

    Add env support for pb base url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss"],
+  runtimeConfig: {
+    public: {
+      pbBaseUrl: "http://127.0.0.1:8090", // can be overridden by NUXT_PUBLIC_PB_BASE_URL environment variable
+    },
+  },
 });
